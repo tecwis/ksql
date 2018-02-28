d25a423d227271296cb76513a9954767f2853ad7
Rohan Desai <desai.p.rohan@gmail.com>
2018-02-28 13:54:48 -0800

    Remove unused interface

--- ksql-common/src/main/java/io/confluent/ksql/metrics/MetricCollector.java
+++ ksql-common/src/main/java/io/confluent/ksql/metrics/MetricCollector.java
@@ -80,6 +80,4 @@ interface MetricCollector extends ConsumerInterceptor, ProducerInterceptor {
   default double totalBytesConsumption() {
     return 0;
   }
-
-  default Map<String, Double> totalMessageConsumptionByTopic() { return null; }
 }
